a081790867f45a8356718351053d0c3bdad847ae
BlauRick <157022879+BlauRick@users.noreply.github.com>
2025-01-23 09:20:51 +0100

    Update szamolas.js

--- szamolas.js
+++ szamolas.js
@@ -1,53 +1,53 @@
-const aramDij=14.2; //áram éves
-const emAramDij=15.08;
-const aramLimit=1320;
+const aramDij = 14.2; //áram éves
+const emAramDij = 15.08;
+const aramLimit = 1320;
 
-const aramUpper=25000
+const aramUpper = 25000
 
-const gazDij=85.06;
-const emGazDij=650.87;
-const gazLimit=149.91;
+const gazDij = 85.06;
+const emGazDij = 650.87;
+const gazLimit = 149.91;
 
-const gazUpper=500;
+const gazUpper = 500;
 
-const alVizDij=179.95;
-const vizDij=218.95;
-const csDij=526.4;
-const emVizDij=504.06;
-const vizLimit=20;
+const alVizDij = 179.95;
+const vizDij = 218.95;
+const csDij = 526.4;
+const emVizDij = 504.06;
+const vizLimit = 20;
 
-const vizUpper=500;
+const vizUpper = 500;
 
-const koltsegLower=6000;
-const koltsegUpper=22000;
+const koltsegLower = 6000;
+const koltsegUpper = 22000;
 
 var aramInput, vizInput, gazInput, koltsegInput, aramEredmeny, vizEredmeny, gazEredmeny, osszEredmeny;
 
 
-function minimum(n1,n2){
-    if(n1<=n2){return n1;}
-    if(n1>n2){return n2;}
+function minimum(n1, n2) {
+    if (n1 <= n2) { return n1; }
+    if (n1 > n2) { return n2; }
 }
-function maximum(n1,n2){
-    if(n1>=n2){return n1;}
-    if(n1<n2){return n2;}
+function maximum(n1, n2) {
+    if (n1 >= n2) { return n1; }
+    if (n1 < n2) { return n2; }
 }
 
-function szamolas(){
-    
+function szamolas() {
 
-    koltsegInput=10000;
-    aramInput=1500; //example values
-    vizInput=21;
-    gazInput=33;
 
-    aramEredmeny=(aramDij*minimum(aramInput*12,aramLimit)+emAramDij*maximum(0,aramInput*12-aramLimit))/12;
-    gazEredmeny=gazDij*minimum(gazInput,gazLimit)+emGazDij*maximum(0,gazInput-gazLimit);
-    vizEredmeny=alVizDij+(vizDij+csDij)*minimum(vizInput,vizLimit)+(emVizDij+csDij)*maximum(0,vizInput-vizLimit);
+    koltsegInput = 10000;
+    aramInput = 1500; //example values
+    vizInput = 21;
+    gazInput = 33;
 
-    osszEredmeny=koltsegInput+aramEredmeny+gazEredmeny+vizEredmeny;
+    aramEredmeny = (aramDij * minimum(aramInput * 12, aramLimit) + emAramDij * maximum(0, aramInput * 12 - aramLimit)) / 12;
+    gazEredmeny = gazDij * minimum(gazInput, gazLimit) + emGazDij * maximum(0, gazInput - gazLimit);
+    vizEredmeny = alVizDij + (vizDij + csDij) * minimum(vizInput, vizLimit) + (emVizDij + csDij) * maximum(0, vizInput - vizLimit);
 
-    document.getElementById("bruh").value=osszEredmeny;
+    osszEredmeny = koltsegInput + aramEredmeny + gazEredmeny + vizEredmeny;
+
+    document.getElementById("bruh").value = osszEredmeny;
 
 
 
@@ -58,8 +58,38 @@ var output_page = "<button onclick=\"load_input_page()\">back</button><br>";
 
 function load_output_page() {
     input_page = document.getElementById("body").innerHTML;
-    output_page = 
+    output_page += output_page +=
+    "<table>" +
+        "<tr>" +
+            "<th></th>" +
+            "<th>Alap díjak</th>" +
+            "<th>Kedvezményes díjak</th>"+
+            "<th>Emelt díjak</th>" +
+            "<th>Alap Költség</th>" +
+            "<th>Emelt költség</th>" +
+            "<th>Összesen</th>" +
+        "</tr>" +
+        "<tr>" +
+            "<th>Áram</th>" +
+            "<td> - </td>" +
+            "<td>" + aramDij + "Ft/kWh</td>" +
+            "<td>" + emAramDij + "Ft/kWh (\\>" + aramLimit +" kWh)</td>"+
+            "<td>" + (aramDij*minimum(aramInput*12,aramLimit))/12 + "</td>"+
+            "<td>" + (emAramDij*maximum(0,aramInput*12-aramLimit))/12 + "</td>"+
+            "<td>" + aramEredmeny + "</td>"+
+        "</tr>" +
+            "<th>Gáz/th>" +
+            "<td> - </td>" +
+            "<td>" + gazDij + "Ft/m³</td>" +
+            "<td>" + emGazDij + "Ft/m³ (\\>" + gazLimit +" m³)</td>"+
+            "<td>" + gazDij * minimum(gazInput, gazLimit) + "</td>"+
+            "<td>" + emGazDij * maximum(0, gazInput - gazLimit) + "</td>"+
+            "<td>" + gazEredmeny + "</td>"+
+        "</tr>";
+
+
     document.getElementById("body").innerHTML = output_page;
+    output_page = "<button onclick=\"load_input_page()\">back</button><br>";
 }
 
 function load_input_page() {
